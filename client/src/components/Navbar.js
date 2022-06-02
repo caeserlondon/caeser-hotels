df31aa288b5fcfafc06ec3a3638fe556a104316a
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-02 22:26:05 +0100

    Updating the Navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,42 @@ import React from 'react';
 function Navbar() {
 	return (
 		<div>
-			<h1>Navbar Component</h1>
+			<nav className="navbar navbar-expand-lg">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="/">
+						Caeser's Hotels
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNav"
+						aria-controls="navbarNav"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarNav">
+						<ul className="navbar-nav mr-auto">
+							<li className="nav-item">
+								<a
+									className="nav-link active"
+									aria-current="page"
+									href="/register"
+								>
+									Register
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="/login">
+									Login
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
 		</div>
 	);
 }
