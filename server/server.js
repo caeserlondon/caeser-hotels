c9e5fde82b92cf7b8d2c8969689bca4d25d3fa0a
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-02 23:33:42 +0100

    Fixing a typo and Adding a console log

--- server/server.js
+++ server/server.js
@@ -4,4 +4,4 @@ const app = express();
 
 const port = process.env.PORT || 5001;
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port, () => console.log(`Server running on port ${port}`));
