3f319db0f3435c4217ca2e147b660d657f95f57c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-03 15:10:55 +0100

    Adding the database configuration

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+const dbConfig = require('./dbConfig.js');
+
 const port = process.env.PORT || 5001;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+	console.log(`Server running on port ${port} Using Nodemon)`)
+);
