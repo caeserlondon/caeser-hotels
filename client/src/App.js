9b0b1a348e6b2ac98c7570f11309697933aa465e
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-03 23:25:17 +0100

    Adding the home screen to the App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<BrowserRouter>
+				{/* <Route path="/home" exact component={HomeScreen} /> */}
+				<Route path="/home" element={<HomeScreen />} />
+			</BrowserRouter>
 		</div>
 	);
 }
