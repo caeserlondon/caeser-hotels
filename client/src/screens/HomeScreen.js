b151ff1a4bf8c79b5455e8c719bd85a2612984eb
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-04 17:29:58 +0100

    Connecting the Data to the homeScreen

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const HomeScreen = () => {
+	const [rooms, setRooms] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const data = (await axios.get('/api/rooms/getallrooms')).data;
+
+				console.log(data);
+				setRooms(data);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		fetchData();
+	}, []);
+
 	return (
 		<div>
 			<h1>Home Screen</h1>
+			<h1>there are {rooms.length} rooms </h1>
 		</div>
 	);
 };
