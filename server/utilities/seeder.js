42ae16b79763220ad3c56504c765bd6f5b90f608
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-04 22:24:26 +0100

    Fixing a typo

--- server/utilities/seeder.js
+++ server/utilities/seeder.js
@@ -1,15 +1,18 @@
-const Room = require('../models/room');
+const room = require('../models/room');
 const mongoose = require('mongoose');
 const rooms = require('../data/rooms');
 
-mongoose.connect();
+mongoose.connect(
+	`mongodb+srv://caeserlondon:caeserlondon@cluster0.hu2ee.mongodb.net/caeser-hotels`
+	// `mongodb+srv://caeserlondon:caeserlondon@cluster0.hu2ee.mongodb.net/?retryWrites=true&w=majority`
+);
 
 const seedRooms = async () => {
 	try {
-		await Room.deleteMany();
+		await room.deleteMany();
 		console.log('Rooms are deleted');
 
-		await Room.insertMany(rooms);
+		await room.insertMany(rooms);
 		console.log('All Rooms are added.');
 
 		process.exit();
