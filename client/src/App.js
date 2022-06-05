223cde33b049cd33f9ecf550c3e02b43ecf982ca
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-05 21:11:39 +0100

    Adding the BookingScreen Route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import Bookingscreen from './screens/BookingScreen';
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/home" element={<HomeScreen />} />
+					<Route path="/book/:roomid" element={<Bookingscreen />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
