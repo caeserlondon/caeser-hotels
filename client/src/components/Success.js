2588984609e17d34d726d45218fa2bdedb617a16
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-05 23:34:23 +0100

    Creating the Success component

---
+++ client/src/components/Success.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Success = () => {
+	return (
+		<div>
+			<div className="alert alert-success" role="alert">
+				A simple success alert—check it out!
+			</div>
+		</div>
+	);
+};
+
+export default Success;
