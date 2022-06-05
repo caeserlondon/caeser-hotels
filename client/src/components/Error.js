9e07373c57c597a3a357c560da4a25fcc0dfbfd8
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-05 23:33:57 +0100

    Creating the Error component

---
+++ client/src/components/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+	return (
+		<div>
+			<div className="alert alert-danger" role="alert">
+				Something went wrong, Please try again later ...
+			</div>
+		</div>
+	);
+};
+
+export default Error;
