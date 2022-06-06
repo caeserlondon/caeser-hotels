363d168358e39c3fe2b4604b89f9672b2b0fa910
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-06 21:58:45 +0100

    Cleaning Up and deleting useState

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,18 +1,15 @@
-import { useState } from 'react';
 import { css } from '@emotion/react';
 import FadeLoader from 'react-spinners/FadeLoader';
 
 const override = css`
 	display: block;
-	margin: 300px auto;
+	margin: 350px auto;
 `;
 
 const Loader = () => {
-	let [loading, setLoading] = useState(true);
-
 	return (
 		<div className="sweet-loading">
-			<FadeLoader color="#D0021B" loading={loading} css={override} size={350} />
+			<FadeLoader color="#D0021B" loading={true} css={override} size={350} />
 		</div>
 	);
 };
