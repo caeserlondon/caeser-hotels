b7f2d74da73bc436edd8fbc724d0d114cb6c00af
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-06 23:44:44 +0100

    Adding the usersRoute

--- server/server.js
+++ server/server.js
@@ -6,9 +6,13 @@ const dbConfig = require('./dbConfig.js');
 
 const roomsRoute = require('./routes/roomsRoute');
 
+const usersRoute = require('./routes/usersRoute');
+
 app.use(express.json());
 
 app.use('/api/rooms', roomsRoute);
+
+app.use('/api/users', usersRoute);
 
 const port = process.env.PORT || 5001;
 
