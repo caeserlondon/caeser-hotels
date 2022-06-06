fddc8fcf35db0695f7775ff34ad31f03ad8c7762
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-06 22:42:36 +0100

    Creating the form input on the Login Screen

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -1,9 +1,47 @@
-function LoginScreen() {
+import { useState } from 'react';
+
+const LoginScreen = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const Login = () => {
+		const user = {
+			email,
+			password,
+		};
+		console.log(user);
+	};
+
 	return (
 		<div>
-			<h1>LoginScreen</h1>
+			<div className="row justify-content-center mt-5">
+				<div className="col-md-5">
+					<div>
+						<h3>Login</h3>
+
+						<input
+							type="email"
+							className="form-control"
+							placeholder="Email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+
+						<button className="btn btn-primary" onClick={Login}>
+							Login
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
-}
+};
 
 export default LoginScreen;
