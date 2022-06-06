c483fcac485baa1999460b02ac0bf4b3752b1af8
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-06 22:42:56 +0100

    Creating the form input on the Register Screen

--- client/src/screens/RegisterScreen.js
+++ client/src/screens/RegisterScreen.js
@@ -1,7 +1,65 @@
+import { useState } from 'react';
+
 const RegisterScreen = () => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+
+	const register = () => {
+		if (password === confirmPassword) {
+			const user = {
+				name,
+				email,
+				password,
+				confirmPassword,
+			};
+			console.log(user);
+		} else {
+			alert('Please enter matching passwords');
+		}
+	};
+
 	return (
 		<div>
-			<h1>RegisterScreen</h1>
+			<div className="row justify-content-center mt-5">
+				<div className="col-md-5">
+					<div>
+						<h3>Register</h3>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="name"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+						<input
+							type="email"
+							className="form-control"
+							placeholder="Email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Confirm Password"
+							value={confirmPassword}
+							onChange={(e) => setConfirmPassword(e.target.value)}
+						/>
+						<button className="btn btn-primary" onClick={register}>
+							Register
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
