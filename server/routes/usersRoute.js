852854a13e33c5fbfae51dd50f2b32097d9a2a76
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-09 22:42:45 +0100

    Deleting the semicolon and duble quotes

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
 
 // --- REGISTER ---
 router.post('/register', async (req, res) => {
-	const { name, email, password } = req.body;
+	const { name, email, password } = req.body
 
-	const newUser = new User({ name, email, password });
+	const newUser = new User({ name, email, password })
 
 	try {
-		newUser.save();
-		res.send('User Registered successfully');
+		newUser.save()
+		res.send('User Registered successfully')
 	} catch (error) {
-		return res.status(400).json({ message: error });
+		return res.status(400).json({ message: error })
 	}
-});
+})
 
 //  --- LOGIN ---
 router.post('/login', async (req, res) => {
-	const { email, password } = req.body;
+	const { email, password } = req.body
 
 	try {
-		const user = await User.findOne({ email: email, password: password });
+		const user = await User.findOne({ email: email, password: password })
 
 		if (user) {
 			const currentUser = {
@@ -29,14 +29,14 @@ router.post('/login', async (req, res) => {
 				email: user.email,
 				isAdmin: user.isAdmin,
 				_id: user._id,
-			};
-			res.send(currentUser);
+			}
+			res.send(currentUser)
 		} else {
-			return res.status(400).json({ message: 'User Login Failed' });
+			return res.status(400).json({ message: 'User Login Failed' })
 		}
 	} catch (error) {
-		return res.status(400).json({ message: 'Something went weong' });
+		return res.status(400).json({ message: 'Something went weong' })
 	}
-});
+})
 
-module.exports = router;
+module.exports = router
