59048f5132aa33537c0e871e09eda42e2080d36a
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-09 22:42:16 +0100

    Deleting the semicolon and duble quotes

--- server/routes/BookingsRoutes.js
+++ server/routes/BookingsRoutes.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const BookingsRoutes = () => {
 	return (
 		<div>
 			<h1>BookingsRoutes</h1>
 		</div>
-	);
-};
+	)
+}
 
-export default BookingsRoutes;
+export default BookingsRoutes
