af49d0e86c25feb5b20458fe58d354fa24ec9bd1
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-09 22:36:37 +0100

    Deleting the semicolon and duble quotes

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -1,11 +1,11 @@
 const Success = ({ message }) => {
 	return (
 		<div>
-			<div className="alert alert-success" role="alert">
+			<div className='alert alert-success' role='alert'>
 				{message}
 			</div>
 		</div>
-	);
-};
+	)
+}
 
-export default Success;
+export default Success
