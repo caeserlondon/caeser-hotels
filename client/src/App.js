08398cee14519594223fb1db8503b0ffecef9ebd
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-09 22:35:29 +0100

    Deleting the semicolon and duble quotes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import BookingScreen from './screens/BookingScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import LoginScreen from './screens/LoginScreen';
+import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import BookingScreen from './screens/BookingScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import LoginScreen from './screens/LoginScreen'
 
 function App() {
 	return (
-		<div className="App">
+		<div className='App'>
 			<Navbar />
 			<BrowserRouter>
 				<Routes>
-					<Route path="/home" element={<HomeScreen />} />
-					<Route path="/book/:roomid" element={<BookingScreen />} />
-					<Route path="/register" element={<RegisterScreen />} />
-					<Route path="/login" element={<LoginScreen />} />
+					<Route path='/home' element={<HomeScreen />} />
+					<Route path='/book/:roomid' element={<BookingScreen />} />
+					<Route path='/register' element={<RegisterScreen />} />
+					<Route path='/login' element={<LoginScreen />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
