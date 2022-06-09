bbbb28fa7edc1ae53e3b980cf758a4e063e8af7a
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-09 22:32:17 +0100

    Deleting the semicolon and duble quotes

--- client/src/screens/BookingScreen.js
+++ client/src/screens/BookingScreen.js
@@ -1,52 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import Loader from '../components/Loader';
-import Error from '../components/Error';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import Loader from '../components/Loader'
+import Error from '../components/Error'
 
-function Bookingscreen() {
-	const [loading, setLoading] = useState(true);
-	const [setError] = useState();
-	const [room, setRoom] = useState();
-	let { roomid } = useParams();
+function Bookingscreen(props) {
+	const [loading, setLoading] = useState(true)
+	const [setError] = useState()
+	const [room, setRoom] = useState()
+	let { roomid } = useParams()
 
 	useEffect(() => {
 		async function fetchData() {
 			try {
-				setLoading(true);
+				setLoading(true)
 				const data = await (
 					await axios.post('/api/rooms/getroombyid', { roomid })
-				).data;
+				).data
 				// console.log(data);
-				setRoom(data);
-				setLoading(false);
+				setRoom(data)
+				setLoading(false)
 			} catch (error) {
-				setError(true);
-				console.log(error);
-				setLoading(false);
+				setError(true)
+				console.log(error)
+				setLoading(false)
 			}
 		}
-		fetchData();
-	}, [roomid, setError]);
+		fetchData()
+	}, [roomid, setError])
+	console.log(props.name)
 
 	return (
-		<div className="booking-container">
+		<div className='booking-container'>
 			{loading ? (
 				<Loader />
 			) : room ? (
-				<div className="row booking-card">
-					<div className="col-md-6">
+				<div className='row booking-card'>
+					<div className='col-md-6'>
 						<h3>{room.name}</h3>
 						<img
 							src={room.imageurls[0]}
 							alt={room.name}
-							className="big-image"
+							className='big-image'
 						/>
 					</div>
-					<div className="col-md-6">
+					<div className='col-md-6'>
 						<div style={{ textAlign: 'right' }}>
 							<h3>Booking Details</h3>
-							<hr className="horizontal-line" />
+							<hr className='horizontal-line' />
 							<p>Name : </p>
 							<p>From Date : </p>
 							<p>To Date : </p>
@@ -55,23 +56,23 @@ function Bookingscreen() {
 					</div>
 					<div style={{ textAlign: 'right' }}>
 						<h3>Price To Pay</h3>
-						<hr className="horizontal-line" />
+						<hr className='horizontal-line' />
 						<p>Total Nights</p>
 						<p>Daily Rate : {room.pricePerNight}</p>
 						<p>Total Amount</p>
 					</div>
 					<div style={{ float: 'right' }}>
-						<a href="/home" className="btn btn-dark m-2">
+						<a href='/home' className='btn btn-dark m-2'>
 							Main Page
 						</a>
-						<button className="btn btn-dark">Pay Now</button>
+						<button className='btn btn-dark'>Pay Now</button>
 					</div>
 				</div>
 			) : (
 				<Error />
 			)}
 		</div>
-	);
+	)
 }
 
-export default Bookingscreen;
+export default Bookingscreen
