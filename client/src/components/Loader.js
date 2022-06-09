79bc0ea48189ef185989c86e09d506cf17270f89
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-09 22:36:14 +0100

    Deleting the semicolon and duble quotes

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,19 +1,19 @@
 // https://www.npmjs.com/package/react-spinners
 
-import { css } from '@emotion/react';
-import CircleLoader from 'react-spinners/CircleLoader';
+import { css } from '@emotion/react'
+import CircleLoader from 'react-spinners/CircleLoader'
 
 const override = css`
 	display: block;
 	margin: 350px auto;
-`;
+`
 
 const Loader = () => {
 	return (
-		<div className="sweet-loading">
-			<CircleLoader color="#a49366" loading={true} css={override} size={200} />
+		<div className='sweet-loading'>
+			<CircleLoader color='#a49366' loading={true} css={override} size={200} />
 		</div>
-	);
-};
+	)
+}
 
-export default Loader;
+export default Loader
