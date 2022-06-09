6b99fcc59a2ada9a7c8ad6866fd0c9530116b794
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-06-09 22:32:43 +0100

    Deleting the semicolon and duble quotes

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,75 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Room from '../components/Room';
-import Loader from '../components/Loader';
-import Error from '../components/Error';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Room from '../components/Room'
+import Loader from '../components/Loader'
+import Error from '../components/Error'
 
 // import { DatePicker, Space } from 'antd';
-import { DatePicker } from 'antd';
-import moment from 'moment';
+import { DatePicker } from 'antd'
+import moment from 'moment'
 
 const HomeScreen = () => {
-	const [rooms, setRooms] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [setError] = useState();
+	const [rooms, setRooms] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [setError] = useState()
 
-	const { RangePicker } = DatePicker;
-	const [fromDate, setFromDate] = useState();
-	const [toDate, setToDate] = useState();
+	const { RangePicker } = DatePicker
+	const [fromDate, setFromDate] = useState()
+	const [toDate, setToDate] = useState()
 
 	useEffect(() => {
 		async function fetchData() {
 			try {
-				setLoading(true);
-				const data = (await axios.get('/api/rooms/getallrooms')).data;
+				setLoading(true)
+				const data = (await axios.get('/api/rooms/getallrooms')).data
 				// console.log(data);
-				setRooms(data);
-				setLoading(false);
+				setRooms(data)
+				setLoading(false)
 			} catch (error) {
-				setError(true);
-				console.log(error);
-				setLoading(false);
+				setError(true)
+				console.log(error)
+				setLoading(false)
 			}
 		}
-		fetchData();
+		fetchData()
 		// }, []);
-	}, [setError, setLoading]);
+	}, [setError, setLoading])
 
 	const filterByDate = (dates) => {
-		setFromDate(moment(dates[0]).format('DD-MM-YYYY'));
-		setToDate(moment(dates[1]).format('DD-MM-YYYY'));
-	};
+		setFromDate(moment(dates[0]).format('DD-MM-YYYY'))
+		setToDate(moment(dates[1]).format('DD-MM-YYYY'))
+	}
 
 	return (
-		<div className="container">
-			<div className="row mt-5 ">
-				<div className="col-md-4">
+		<div className='container'>
+			<div className='row mt-5 '>
+				<div className='col-md-4'>
 					<RangePicker
-						format="DD-MM-YYYY"
-						className="range-picker"
+						format='DD-MM-YYYY'
+						className='range-picker'
 						onChange={filterByDate}
 					/>
 				</div>
 			</div>
 
-			<section className="row justify-content-center mt-5">
+			<section className='row justify-content-center mt-5'>
 				{loading ? (
 					<Loader />
 				) : rooms.length > 1 ? (
 					rooms.map((room) => {
 						// console.log(room);
 						return (
-							<article className="col-md-9 mt-2" key={room._id}>
+							<article className='col-md-9 mt-2' key={room._id}>
 								<Room room={room} fromDate={fromDate} toDate={toDate} />
 							</article>
-						);
+						)
 					})
 				) : (
 					<Error />
 				)}
 			</section>
 		</div>
-	);
-};
+	)
+}
 
-export default HomeScreen;
+export default HomeScreen
